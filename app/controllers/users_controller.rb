eb6d6b4d5f8a9397e8b26707ee7cac2be3d96be5
EC2 Default User <ec2-user@ip-172-31-11-229.ap-northeast-1.compute.internal>
2022-10-26 05:01:26 +0000

    [Bookers2Ver2]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update,:edit]
+  before_action :set_user, only: [:followings, :followers]
 
   def show
     @user = User.find(params[:id])
@@ -24,11 +25,23 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
+  
+  def followings
+    @users = @user.followings
+  end
+  
+  def followers
+    @users = @user.followers
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def ensure_correct_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 devise_for :users
 
@@ -10,8 +12,11 @@ devise_for :users
     resources :book_comments, only: [:create,:destroy]
     
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  
+  resources :users, only: [:index,:show,:edit,:update,:followers,:followings] do
+    resource :relationships, only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
