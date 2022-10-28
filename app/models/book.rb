03cc1ab67b046ef3c89442e4ee927bac1b195c47
EC2 Default User <ec2-user@ip-172-31-11-229.ap-northeast-1.compute.internal>
2022-10-28 07:44:28 +0000

    [Bookers2ver2]

--- app/models/book.rb
+++ app/models/book.rb
@@ -25,6 +25,11 @@ class Book < ApplicationRecord
   
   scope :created_today, -> { where(created_at: Time.current.all_day) }
   scope :created_yesterday, -> { where(created_at: Time.zone.yesterday.all_day) }
+  scope :created_2days, -> { where(created_at: 2.days.ago.all_day) }
+  scope :created_3days, -> { where(created_at: 3.days.ago.all_day) }
+  scope :created_4days, -> { where(created_at: 4.days.ago.all_day) }
+  scope :created_5days, -> { where(created_at: 5.days.ago.all_day) }
+  scope :created_6days, -> { where(created_at: 6.days.ago.all_day) }
   scope :created_thisweek, -> { where(created_at: Time.current.all_week) }
   scope :created_lastweek, -> { where(created_at: Time.current.last_week.all_week) }
   
