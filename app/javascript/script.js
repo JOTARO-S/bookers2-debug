1bf7da8edbaf175ce9109b4d026061c0f6718508
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-11-08 08:01:52 +0000

    [Bookers2ver2] 8d 1

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,16 +1,4 @@
 /*  global $*/
-$(document).on("turbolinks:load", function() {
-  $(function() {
-    $(".tab").click(function(){
-      $(".tab-active").removeClass("tab-active");
-      $(this).addClass("tab-active");
-      $(".box-show").removeClass("box-show");
-      const index = $(this).index();
-      $(".tabbox").eq(index).addClass("box-show");
-    });
-  }); 
-});
-
 $('#tab-contents .tab[id != "tab1"]').hide();
 
 $('#tab-menu a').on('click', function(event) {
